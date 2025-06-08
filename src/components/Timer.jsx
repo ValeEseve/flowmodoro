58e6aa41547e7e88c231254dd27b44ab10e781bd
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-06-08 01:17:27 -0400

    Fin sesión nocturna 6/6/25

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -7,28 +7,39 @@ const Timer = () => {
     const timerOnEmoji = "▶️"
     const timerOffEmoji = "⏸️"
 
-    const timePlay = () => { 
-        setTimerOn(prev => !prev) 
-        console.log(Boolean(timerOn))}
+    const timePlay = () => {
+        setTimerOn(prev => !prev)
+        console.log(Boolean(timerOn))
+    }
+
+    const addTime=()=> {
+        setTime((prev) => (prev + 1000))
+    }
 
     useEffect(() => {
-        if (!timerOn) { return }
-        const intervalo = setInterval(() => {
-            if (time < 0) { return
-       
-        } else {
-             setTime( (prev) => prev - 1000 )
-        console.log(time)
-        }
-        }, 1000);
-        return () => {clearInterval(intervalo)}
+        
 
+        if (!timerOn) { return }
+
+        const intervalo = setInterval(() => { 
+        setTime((prev) => {
+            if (prev <= 0){
+                clearInterval(intervalo)
+                setTimerOn(false)
+                return 0
+            }
+            return prev - 1000
+            })         
+        }, 1000)
+
+        return () => { clearInterval(intervalo) }
     }, [timerOn])
 
     return (
         <div>
             <h1>Tiempo restante: {parseFloat(time / 1000)} segundos</h1>
             <button onClick={timePlay} style={{ fontSize: "60px", padding: "0px" }}>{timerOn ? timerOffEmoji : timerOnEmoji}</button>
+            <button onClick={addTime} style={{backgroundColor:"chocolate"}}>Añadir 1 segundo</button>
         </div>
     )
 }
